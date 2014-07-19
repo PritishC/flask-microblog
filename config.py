22f1f0dcae113680249859b2c12582c5b05b9aca
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-19 20:19:57 +0530

    Created a configuration file. Contains a CSRF_ENABLED token and a SECRET_KEY token.

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 19 20:18:43 2014
+
+@author: pritishc
+"""
+
+CSRF_ENABLED = True #Cross-site request forgery prevention
+SECRET_KEY = 'dbz3:Treemight'
