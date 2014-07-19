b389a5de5da336158550e0af8d9dd68b59e442a3
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-19 22:35:45 +0530

    Added a validate_on_submit() method call. It does form processing: if it is called when form is being presented to the user (before they get a chance to input shit) it will return False, so the template has to be rendered. If it is called as part of a form submission request, it gathers data, runs validators attached to fields, and returns True if all okay. If any one field fails during validation then it returns False. Error messages will be added later. flash function shows a message on next page presented to the user. redirect function tels the browser to navigate to a different web page than the one requested.

--- app/views.py
+++ app/views.py
@@ -30,6 +30,9 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + 
+        '", remember_me=' + str(form.remember_me.data))
     return render_template('login.html',
                            title='Sign In',
                            form=login_form)
