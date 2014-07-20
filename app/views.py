849ae6a98dc1cab060cdda44979011bd2182dc1d
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-20 19:40:48 +0530

    Made changes to the views.py file again. Imported a shit ton of modules. The 'g' global object is setup by Flask to store and share data during the life of a request. The logged in user is stored here. The url_for function is defined by Flask as a clean way to obtain the URL for a given view function. Let Flask do the URL building. The flask session object differs from the db.session object. Once data is stored in this object, it is available during that request and any future requests from the *same client*. Data remains until explicitly removed. Flask keeps a different session file for each client of the app. oid.try_login call triggers user auth via Flask-OpenID. It takes the openid given by the user and a list of items we require from the OpenID provider (nickname and email here). OpenID auth happens asynchronously. Flask-OpenID calls a function registered with @oid.after_login decorator if auth is successful. If not, user is taken back to the login page.

--- app/views.py
+++ app/views.py
@@ -4,9 +4,11 @@ Created on Sat Jul 19 19:23:42 2014
 
 @author: pritishc
 """
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request,g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/')
 @app.route('/index')
@@ -28,11 +30,15 @@ def index():
                            posts=posts)
                            
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        flash('Login requested for OpenID="' + login_form.openid.data + 
-        '", remember_me=' + str(login_form.remember_me.data))
+        session['remember_me'] = login_form.remember_me.data
+        return oid.try_login(login_form.openid.data, ask_for = ['nickname',
+                                                                'email'])
     return render_template('login.html',
                            title='Sign In',
                            form=login_form,
