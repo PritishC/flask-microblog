340de2d5f5e9ed44438803652d64aed6451a110b
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-20 10:30:32 +0530

    Changed the login template to display error messages. Form field validation errors are usually found in form.errors.fieldname.

--- app/views.py
+++ app/views.py
@@ -30,9 +30,9 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + 
-        '", remember_me=' + str(form.remember_me.data))
+    if login_form.validate_on_submit():
+        flash('Login requested for OpenID="' + login_form.openid.data + 
+        '", remember_me=' + str(login_form.remember_me.data))
     return render_template('login.html',
                            title='Sign In',
                            form=login_form)
