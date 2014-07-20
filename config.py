7f18aca574e1a07d857829cf48e183b8acc506e8
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-20 11:08:36 +0530

    Changes to config.py. 1. SQLALCHEMY_DATABASE_URI is required by flask-sqlalchemy. 2. SQLALCHEMY_MIGRATE_REPO is the folder where SQLAlchemy-migrate data files are stored. 3. We use sqlite for smaller applications. DB is stored in a single file named app.db here. 4. The ORM helps us to skip writing SQL and write in object-oriented lingo instead.

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ Created on Sat Jul 19 20:18:43 2014
 
 @author: pritishc
 """
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 CSRF_ENABLED = True #Cross-site request forgery prevention
 SECRET_KEY = 'dbz3:Treemight'
