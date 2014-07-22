e29218e21327ca973051f0ab44c8ae97e2b97911
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-22 10:47:42 +0530

    Modified the database model. The User class gets two new fields, about_me and last_seen. Since the database model was modified, it was necessary to migrate the DB to a new version.

--- app/models.py
+++ app/models.py
@@ -17,6 +17,8 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.SmallInteger, default = ROLE_USER)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
     
     def is_authenticated(self):
         return True
