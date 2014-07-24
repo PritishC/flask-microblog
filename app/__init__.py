6ca1a7cd97fc0862be978d098bb12ee0215c860e
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-24 12:06:07 +0530

    Added a RotatingFileHandler to log messages to a file. It has been chosen to preserve space (at 1MB per file) and use the last ten log files as backups. logging.Formatter allows us to custom-format our messages. We lower the logging level both in the app logger and the file logger handler, so as to write useful messages to the log without calling them errors.

--- app/__init__.py
+++ app/__init__.py
@@ -18,10 +18,18 @@ from app import views, models
 
 if not app.debug:
     import logging
-    from logging.handlers import SMTPHandler
+    from logging.handlers import SMTPHandler, RotatingFileHandler
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
     mail_handler = STMPHandler((MAIL_SERVER, MAIL_PORT), 'no_reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
     mail_hander.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+    
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s\
+    [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
