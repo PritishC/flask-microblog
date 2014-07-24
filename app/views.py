b4c8cb9812a7d39a82b37d21eb5c6e7812d59095
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-24 09:50:09 +0530

    Added a view function for profile edits. Either the user enters data in the form and it is saved in Flask's g object, or the form gets its data from the g object. Template rendering then occurs.

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from flask import render_template, flash, redirect, session, url_for, request,g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm
+from forms import LoginForm, EditForm
 from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/')
@@ -65,6 +65,22 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+@app.route('/edit', methods=['GET', 'POST'])                           
+@login_required
+def edit():
+    form = EditForm()
+        if form.validate_on_submit():
+            g.user.nickname = form.nickname.data
+            g.user.about_me = form.about_me.data
+            db.session.add(g.user)
+            db.session.commit()
+            flash('Your changes have been saved')
+            return redirect(url_for('edit'))
+        else:
+            form.nickname.data = g.user.nickname
+            form.about_me.data = g.user.about_me
+        return render_template('edit.html', form=form)
 
 @oid.after_login
 def after_login(resp):
