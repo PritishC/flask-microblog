400a76f35632e19aae56236995abf4820168ba57
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-24 12:17:31 +0530

    Bug Fix Part I. After the user has logged in, the nickname is generated by the server. At this point, the server needs to check whether it already has this nickname in the DB. If it does, it needs to return a unique nickname. If not, the nickname generated by splicing the email ID is fine. The uniqueness is maintained by adding a counter to the nickname and checking the DB if this nickname exists. Eventually we get a unique nick.

--- app/views.py
+++ app/views.py
@@ -102,7 +102,7 @@ def after_login(resp):
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
+        user = User.make_unique_nickname(nickname)
         db.session.add(user)
         db.session.commit()
     remember_me = False
