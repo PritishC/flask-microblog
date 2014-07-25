fed31010730a0f1ac2c68c42b7bbb1129177872d
VaticanCameos <chakrabortypritish@gmail.com>
2014-07-25 12:01:41 +0530

    Added tests for testing the followed posts mechanism.

--- test_framework.py
+++ test_framework.py
@@ -7,10 +7,11 @@ Created on Thu Jul 24 12:58:49 2014
 
 import os
 import pytest
+from datetime import datetime, timedelta
 
 from config import basedir
 from app import app, db
-from app.models import User
+from app.models import User, Post
 
 def setup_function(function):
     app.config['TESTING'] = True
@@ -65,3 +66,44 @@ def test_follow():
     assert u1.is_following(u2) == False
     assert u1.followed.count() == 0
     assert u2.followers.count() == 0
+    
+def test_follow_posts():
+    u1 = User(nickname='john', email='john@example.com')
+    u2 = User(nickname='susan', email='susan@example.com')
+    u3 = User(nickname='mary', email='mary@example.com')
+    u4 = User(nickname='david', email='david@example.com')
+    for u in [u1, u2, u3, u4]:
+        db.session.add(u)
+    utcnow = datetime.utcnow()
+    p1 = Post(body="post from john", author=u1, timestamp=utcnow+timedelta(seconds=1))
+    p2 = Post(body="post from susan", author=u2, timestamp=utcnow+timedelta(seconds=2))
+    p3 = Post(body="post from mary", author=u3, timestamp=utcnow+timedelta(seconds=3))
+    p4 = Post(body="post from david", author=u4, timestamp=utcnow+timedelta(seconds=4))
+    for p in [p1, p2, p3, p4]:
+        db.session.add(p)
+    db.session.commit()
+    # setup followers.
+    # To make the user see their own posts in the stream of followed posts,
+    # we make them follow themselves.
+    u1.follow(u1)
+    u1.follow(u2)
+    u1.follow(u4)
+    u2.follow(u2)
+    u2.follow(u3)
+    u3.follow(u3)
+    u3.follow(u4)
+    u4.follow(u4)
+    for u in [u1, u2, u3, u4]:
+        db.session.add(u)
+    db.session.commit()
+    followed_posts = []
+    for u in [u1, u2, u3, u4]:
+        followed_posts.append(u.followed_posts().all())
+    assert len(followed_posts[0]) == 3
+    assert len(followed_posts[1]) == 2
+    assert len(followed_posts[2]) == 2
+    assert len(followed_posts[3]) == 1
+    assert followed_posts[0] == [p4, p2, p1]
+    assert followed_posts[1] == [p3, p2]
+    assert followed_posts[2] == [p4, p3]
+    assert followed_posts[3] == [p4]
