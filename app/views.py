37803d31c58477729a2c7c6091f4fc6472a8496b
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-13 19:00:42 +0530

    Modified the user view function. This is to bring pagination to the user profile page.

--- app/views.py
+++ app/views.py
@@ -52,16 +52,14 @@ def logout():
     return redirect(url_for('index'))
     
 @app.route('/user/<nickname>')
+@app.route('/user/<nickname>/<int:page>')
 @login_required
-def user(nickname):
+def user(nickname, page=1):
     user = User.query.filter_by(nickname = nickname).first()
     if user == None:
         flash('User' + nickname + ' not found.')
         return redirect(url_for('index'))
-    posts = [
-        { 'author': user, 'body': 'Test post #1'},
-        { 'author': user, 'body': 'Test post #2'}
-    ]
+    posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
     return render_template('user.html',
                            user=user,
                            posts=posts)
