c22e66d981cf024558874934e9228cdc962c2122
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-13 18:45:17 +0530

    Modified the index view function. Added a route decorator for URLS of the type /index/1, /index/2 and so on, depicting pages. The paginate() method returns a Pagination object, whose items attribute has the necessary items, which here is three posts per page.

--- app/views.py
+++ app/views.py
@@ -10,11 +10,13 @@ from flask.ext.login import login_user, logout_user, current_user, login_require
 from app import app, db, lm, oid
 from forms import LoginForm, EditForm, PostForm
 from models import User, ROLE_USER, ROLE_ADMIN, Post
+from config import POSTS_PER_PAGE
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
+@app.route('/index/<int:page>', methods=['GET', 'POST'])
 @login_required
-def index():
+def index(page=1):
     form = PostForm()
     if form.validate_on_submit():
         post = Post(body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
@@ -22,7 +24,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = g.user.followed_posts().all()
+    posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False).items
     return render_template("index.html",
                            title='Home',
                            user=user,
