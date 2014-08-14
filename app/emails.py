ab6e072b947950a0f9fa2c9b30e81af86f3e12f3
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-14 13:28:06 +0530

    Added a follower_notification function to app/emails.py. This sends notifications when someone follows a user. render_template can be used to render text files which have email (text) templates.

--- app/emails.py
+++ app/emails.py
@@ -1,6 +1,8 @@
 __author__ = 'pritishc'
 
 from flask.ext.mail import Message
+from flask import render_template
+from config import ADMINS
 from app import mail
 
 def send_email(subject, sender, recipients, text_body, html_body):
@@ -8,3 +10,12 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+
+def follower_notification(followed, follower):
+    send_email("[microblog] {0} is now following you!".format(follower.nickname),
+               ADMINS[0],
+               [followed.email],
+               render_template("followed_email.txt",
+                               user=followed, follower=follower),
+               render_template("follower_email.txt",
+                               user=followed, follower=follower))
