eea3441a261c5aa4d3467f548459c331555dfb7f
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-14 10:56:15 +0530

    Added Mail object to app/__init__.py.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
@@ -13,6 +14,7 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+mail = Mail(app)
 
 from app import views, models
 
