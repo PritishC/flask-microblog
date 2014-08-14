c170b61778b0d1b68079bbb25ac88bb79632d294
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-14 13:46:30 +0530

    Added a follower_notification call to the follow function in app/views.py.

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ from flask import render_template, flash, redirect, session, url_for, request,g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from forms import LoginForm, EditForm, PostForm, SearchForm
+from emails import follower_notification
 from models import User, ROLE_USER, ROLE_ADMIN, Post
 from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS
 
@@ -97,6 +98,7 @@ def follow(nickname):
     db.session.add(u)
     db.session.commit()
     flash('You are now following ' + nickname + '!')
+    follower_notification(user, g.user)
     return redirect(url_for('user', nickname=nickname))
 
 @app.route('/unfollow/<nickname>')
