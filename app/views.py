3ce4c4ad355382c635556c88e5de4be312b576b4
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-25 22:14:12 +0530

    Added a make_valid_nickname call to the after_login handler in app/views.py.

--- app/views.py
+++ app/views.py
@@ -145,6 +145,7 @@ def after_login(resp):
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
+        nickname = User.make_valid_nickname(nickname)
         nickname = User.make_unique_nickname(nickname)
         user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
         db.session.add(user)
