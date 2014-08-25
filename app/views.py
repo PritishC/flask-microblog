22e5aadbd3a84fc3e2f6bd17d8043fed2ca3ec76
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-25 21:00:03 +0530

    Added gettext calls to all strings inside flash(). app/views.py.

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ Created on Sat Jul 19 19:23:42 2014
 from datetime import datetime
 from flask import render_template, flash, redirect, session, url_for, request,g
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask.ext.babel import gettext
 from app import app, db, lm, oid, babel
 from forms import LoginForm, EditForm, PostForm, SearchForm
 from emails import follower_notification
@@ -27,7 +28,7 @@ def index(page=1):
         post = Post(body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
         db.session.add(post)
         db.session.commit()
-        flash('Your post is now live!')
+        flash(gettext('Your post is now live!'))
         return redirect(url_for('index'))
     posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
     return render_template("index.html",
@@ -62,7 +63,7 @@ def logout():
 def user(nickname, page=1):
     user = User.query.filter_by(nickname = nickname).first()
     if user == None:
-        flash('User' + nickname + ' not found.')
+        flash(gettext('User' + nickname + ' not found.'))
         return redirect(url_for('index'))
     posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
     return render_template('user.html',
@@ -78,7 +79,7 @@ def edit():
         g.user.about_me = form.about_me.data
         db.session.add(g.user)
         db.session.commit()
-        flash('Your changes have been saved')
+        flash(gettext('Your changes have been saved'))
         return redirect(url_for('edit'))
     else:
         form.nickname.data = g.user.nickname
@@ -90,18 +91,18 @@ def edit():
 def follow(nickname):
     user = User.query.filter_by(nickname=nickname).first()
     if user == None:
-        flash('User' + nickname + ' not found.')
+        flash(gettext('User' + nickname + ' not found.'))
         return redirect(url_for('index'))
     if user == g.user:
-        flash('You can\'t follow yourself! pleb')
+        flash(gettext('You can\'t follow yourself! pleb'))
         return redirect(url_for('user', nickname=nickname))
     u = g.user.follow(user)
     if u is None:
-        flash('Cannot follow ' + nickname + '.')
+        flash(gettext('Cannot follow ' + nickname + '.'))
         return redirect(url_for('user', nickname=nickname))
     db.session.add(u)
     db.session.commit()
-    flash('You are now following ' + nickname + '!')
+    flash(gettext('You are now following ' + nickname + '!'))
     follower_notification(user, g.user)
     return redirect(url_for('user', nickname=nickname))
 
@@ -113,15 +114,15 @@ def unfollow(nickname):
         flash('User ' + nickname + ' not found.')
         return redirect(url_for('index'))
     if user == g.user:
-        flash('You can\'t unfollow yourself! [autism intensifies]')
+        flash(gettext('You can\'t unfollow yourself! [autism intensifies]'))
         return redirect(url_for('user', nickname=nickname))
     u = g.user.unfollow(user)
     if u is None:
-        flash('Cannot unfollow '  + nickname + '.')
+        flash(gettext('Cannot unfollow '  + nickname + '.'))
         return redirect(url_for('user', nickname=nickname))
     db.session.add(u)
     db.session.commit()
-    flash('You have unfollowed ' + nickname)
+    flash(gettext('You have unfollowed ' + nickname))
     return redirect(url_for('user', nickname=nickname))
 
 @app.errorhandler(404)
@@ -137,7 +138,7 @@ def internal_error(error):
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
+        flash(gettext('Invalid login. Please try again.'))
         return redirect(url_for('login'))
     user = User.query.filter_by(email = resp.email).first()
     if user is None:
