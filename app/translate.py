79687f9172dbae7c14045b3daaa1d61328db4639
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-30 19:25:57 +0530

    Added a translation source file. It acquires the Microsoft translator access token and attempts translations.

---
+++ app/translate.py
@@ -0,0 +1,36 @@
+__author__ = 'pritishc'
+
+import urllib, httplib
+import json
+from flask.ext.babel import gettext
+from config import MS_TRANSLATOR_CLIENT_ID, MS_TRANSLATOR_CLIENT_SECRET
+
+def microsoft_translate(text, sourceLang, destLang):
+    if MS_TRANSLATOR_CLIENT_ID == '' or MS_TRANSLATOR_CLIENT_SECRET == '':
+        return gettext('Error: translation service not configured')
+    try:
+        # get token
+        params = urllib.urlencode({
+            'client_id': MS_TRANSLATOR_CLIENT_ID,
+            'client_secret': MS_TRANSLATOR_CLIENT_SECRET,
+            'scope': 'http://api.microsofttranslator.com',
+            'grant_type': 'client_credentials',
+        })
+        conn = httplib.HTTPSConnection("datamarket.accesscontrol.windows.net")
+        conn.request("POST", "v2/OAuth2-13", params)
+        response = json.loads(conn.getresponse().read())
+        token = response[u'access_token']
+
+        # translation
+        conn = httplib.HTTPConnection('api.microsofttranslator.com')
+        params = {
+            'appId': 'Bearer' + token,
+            'from': sourceLang,
+            'to': destLang,
+            'text': text.encode('utf-8'),
+        }
+        conn.request("GET", "/V2/Ajax.svc/Translate?" + urllib.urlencode(params))
+        response = json.loads("{\"response\":" + conn.getresponse().read().decode('utf-8-sig') + "}")
+        return response["response"]
+    except:
+        return gettext("Error: Unexpected error in translation mechanism.")
