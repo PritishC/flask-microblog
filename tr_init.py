e071e857bf30fe2b4308d2444f5298650ef6ff82
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-30 14:58:26 +0530

    Added a translate init script.

---
+++ tr_init.py
@@ -0,0 +1,15 @@
+#!/home/pritishc/Envs/ircflask/bin/python
+
+__author__ = 'pritishc'
+import os
+import sys
+if sys.platform == 'win32':
+    pybabel = 'flask\\Scripts\\pybabel'
+else:
+    pybabel = 'ircflask/bin/pybabel'
+if len(sys.argv) != 2:
+    print "usage: tr_init <language-code>"
+    sys.exit(1)
+os.system(pybabel + ' extract -F babel.cfg -k lazy_gettext -o messages.pot app')
+os.system(pybabel + ' init -i messages.pot -d app/translations -l ' + sys.argv[1])
+os.unlink('messages.pot')
