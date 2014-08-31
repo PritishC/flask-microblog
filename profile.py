bccf939186bf1f9ea4c313ceb62948ea3465877a
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-31 13:18:42 +0530

    Re-added the profile script.

--- profile.py
+++ profile.py
@@ -6,5 +6,5 @@ from werkzeug.contrib.profiler import ProfilerMiddleware
 from app import app
 
 app.config['PROFILE'] = True
-app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30]) # 30 functions profiled
-app.run(debug=True)
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions = [30])
+app.run(debug = True)
