c4bde6e1b643298c22bae21fcfaf7919591546fb
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-31 13:20:34 +0530

    Added some SQLAlchemy DB settings to config.py to check DB performance.

--- config.py
+++ config.py
@@ -7,12 +7,17 @@ Created on Sat Jul 19 20:18:43 2014
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+DATABASE_QUERY_TIMEOUT = 0.5
 
+# csrf prevention
 CSRF_ENABLED = True #Cross-site request forgery prevention
 SECRET_KEY = 'dbz3:Treemight'
 
+# openid
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
