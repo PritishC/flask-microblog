537e2b10cca058a547c077471583af4a0697c83d
VaticanCameos <chakrabortypritish@gmail.com>
2014-09-02 01:07:06 +0530

    Changed config.py. Added a line to use HEROKU_POSTGRESQL_[COLOR]_URL env var.

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # database
-if os.environ.get('DATABASE_URL') is None:
+if os.environ.get('HEROKU') is not None:
+    SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_GREEN_URL']
+elif os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
