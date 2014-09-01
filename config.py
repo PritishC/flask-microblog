fb93d11fb0a08e31c04f17c4e3d78ef359495396
VaticanCameos <chakrabortypritish@gmail.com>
2014-09-01 13:00:35 +0530

    Part of Linux Deployment. Added some lines to config.py for database URL setting.

--- config.py
+++ config.py
@@ -8,7 +8,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # database
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_RECORD_QUERIES = True
 DATABASE_QUERY_TIMEOUT = 0.5
