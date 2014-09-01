8eba6c3f3c580a1e9977e260d611dd6548d8d298
VaticanCameos <chakrabortypritish@gmail.com>
2014-09-01 13:27:47 +0530

    Fixing the subprocess calls. The second argument must be nothing or an integer, for buffer size. All strings are to be put in one list.

--- setup.py
+++ setup.py
@@ -5,20 +5,28 @@ if sys.platform == 'win32':
     bin = 'Scripts'
 else:
     bin = 'bin'
-subprocess.call('pip', 'install', 'flask<0.10')
-subprocess.call('pip', 'install', 'flask-login')
-subprocess.call('pip', 'install', 'flask-openid')
-if sys.platform == 'win32':
-    subprocess.call('pip', 'install', '--no-deps', 'lamson', 'chardet', 'flask-mail')
-else:
-    subprocess.call('pip', 'install', 'flask-mail')
-subprocess.call('pip', 'install', 'sqlalchemy==0.7.9')
-subprocess.call('pip', 'install', 'flask-sqlalchemy')
-subprocess.call('pip', 'install', 'sqlalchemy-migrate')
+processlist = ['pip', 'install']
+toinstall = ['flask<0.10', 'flask-login', 'flask-openid', 'flask-mail', 'sqlalchemy==0.7.9',
+             'flask-sqlalchemy', 'sqlalchemy-migrate', 'git+git://github.com/miguelgrinberg/Flask-WhooshAlchemy',
+             'flask-wtf', 'flask-babel', 'guess-language', 'flup', 'coverage']
+
+for install in toinstall:
+    subprocess.call(processlist+[install])
+
+#subprocess.call('pip', 'install', 'flask<0.10')
+#subprocess.call('pip', 'install', 'flask-login')
+#subprocess.call('pip', 'install', 'flask-openid')
+#if sys.platform == 'win32':
+#    subprocess.call('pip', 'install', '--no-deps', 'lamson', 'chardet', 'flask-mail')
+#else:
+#    subprocess.call('pip', 'install', 'flask-mail')
+#subprocess.call('pip', 'install', 'sqlalchemy==0.7.9')
+#subprocess.call('pip', 'install', 'flask-sqlalchemy')
+#subprocess.call('pip', 'install', 'sqlalchemy-migrate')
 #subprocess.call('pip', 'install', 'flask-whooshalchemy'])
-subprocess.call('pip', 'install', 'git+git://github.com/miguelgrinberg/Flask-WhooshAlchemy')
-subprocess.call('pip', 'install', 'flask-wtf')
-subprocess.call('pip', 'install', 'flask-babel')
-subprocess.call('pip', 'install', 'guess-language')
-subprocess.call('pip', 'install', 'flup')
-subprocess.call('pip', 'install', 'coverage')
+#subprocess.call('pip', 'install', 'git+git://github.com/miguelgrinberg/Flask-WhooshAlchemy')
+#subprocess.call('pip', 'install', 'flask-wtf')
+#subprocess.call('pip', 'install', 'flask-babel')
+#subprocess.call('pip', 'install', 'guess-language')
+#subprocess.call('pip', 'install', 'flup')
+#subprocess.call('pip', 'install', 'coverage')
